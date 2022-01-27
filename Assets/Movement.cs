bd902e6106e24a23946a03b24d64d3f82a264040
ins4nepsych0 <90908140+ins4nepsych0@users.noreply.github.com>
2022-01-27 18:28:39 +0100

    v1.6
    updated version

---
+++ Assets/Movement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    Rigidbody rb; 
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector3(xMov, rb.velocity.y, zMov) * speed ;
+
+
+    }
+}
